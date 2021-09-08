58563667892dcdc004a82e573c6dbb8f944803e6
Chris <88003200+Chrioo@users.noreply.github.com>
2021-09-08 08:04:39 -0600

    H

---
+++ .vs/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { private static object print;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ingrese el valor de compra ");
+            double Compra = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("ingrese un numero del 1 al 7 para definir que dia de la semana es  " +
+                " tomando en cuenta que domingo es tiene el valor del numero 1 ");
+            int Dia = int.Parse(Console.ReadLine())
+            Clscompras clsCompra = new Clscompras(Compra, Dia);
+            NClsDescuentos NClsCompra = new NClsDescuentos();
+            Console.WriteLine(NClsCompra.NClsDescuento(clsCompra));
+            Console.ReadLine();
+
+        }
+    }
+}
